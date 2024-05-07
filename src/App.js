a49f38e61e12fd7329a7a90a09882fd2ff570df1
vikash-kanauja <vikashkanoujia@gmail.com>
2024-05-07 19:21:15 +0530

    format code

--- src/App.js
+++ src/App.js
@@ -21,21 +21,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// const PrivateRoute = ({ Component, redirectTo }) => {
-//   const navigate = useNavigate();
-//   useEffect(() => {
-//     const token = JSON.parse(localStorage.getItem("token"));
-//     if (token) {
-//       if (redirectTo === "/signup" || redirectTo === "/") {
-//         navigate("/polling");
-//       }
-//     } else {
-//       navigate(redirectTo === "/signup" ? "/signup" : "/");
-//     }
-//   }, [navigate, redirectTo]);
-//   return <Component />;
-// };
-// export default PrivateRoute;
