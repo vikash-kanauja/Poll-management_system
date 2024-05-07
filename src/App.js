9de5d637f8ce11feeb3c461ac1087498c99943f7
vikash-kanauja <vikashkanoujia@gmail.com>
2024-05-07 19:10:35 +0530

    Fix routing bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import Login from "./pages/Login";
 import Poll from "./pages/Poll";
@@ -6,16 +6,36 @@ import Poll from "./pages/Poll";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PrivateRoute />}>
-            <Route element={<Poll />} path="/poll" />
-          </Route>
-          <Route element={<Login />} path="/" />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<PrivateRoute Component={Login} redirectTo="/" />}
+        />
+        <Route
+          path="/poll"
+          element={<PrivateRoute Component={Poll} redirectTo="/poll" />}
+        />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// const PrivateRoute = ({ Component, redirectTo }) => {
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     const token = JSON.parse(localStorage.getItem("token"));
+//     if (token) {
+//       if (redirectTo === "/signup" || redirectTo === "/") {
+//         navigate("/polling");
+//       }
+//     } else {
+//       navigate(redirectTo === "/signup" ? "/signup" : "/");
+//     }
+//   }, [navigate, redirectTo]);
+//   return <Component />;
+// };
+// export default PrivateRoute;
